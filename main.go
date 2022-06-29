701fa4f8f3cdc4c365041c8d357de23afdcce476
Felipe Azevedo <felipe9_azevedo@hotmail.com>
2022-06-29 19:54:05 -0300

    added better solution

--- main.go
+++ main.go
@@ -16,62 +16,42 @@ type Cache interface {
 	List()
 }
 
-type data struct {
-	key        string
-	data       any
-	expiration int64
-	createdAt  int64
-}
-
 type cache struct {
-	data []data
+	data map[string]any
 }
 
 func NewCache() Cache {
-	c := &cache{}
-	go c.sync()
+	c := &cache{
+		data: make(map[string]any),
+	}
 	return c
 }
 
-func (c *cache) sync() {
-	for {
-		for _, v := range c.data {
-			now := time.Now().UnixNano() / int64(time.Millisecond)
-			if (now - v.createdAt) >= v.expiration {
-				fmt.Printf("removing %s due to expiration time.\n\n", v.key)
-				mu.Lock()
-				c.Unset(v.key)
-				mu.Unlock()
-			}
-		}
-	}
-}
-
 func (c *cache) Add(key string, value any, exp int64) Cache {
-	c.data = append(c.data, data{
-		key:        key,
-		data:       value,
-		expiration: exp,
-		createdAt:  time.Now().UnixNano() / int64(time.Millisecond),
-	})
+	c.data[key] = value
+	go func() {
+		select {
+		case <-time.After(time.Duration(exp) * time.Millisecond):
+			fmt.Printf("%s expired\n", key)
+			_, ok := c.data[key]
+			if ok {
+				c.Unset(key)
+			}
+			return
+		}
+	}()
 	return c
 }
 
 func (c *cache) Unset(key string) Cache {
-	var newData []data
-	for _, v := range c.data {
-		if v.key != key {
-			newData = append(newData, v)
-		}
-	}
-	c.data = newData
+	delete(c.data, key)
 	c.List()
 	return c
 }
 
 func (c *cache) List() {
-	for _, d := range c.data {
-		fmt.Printf("%s: %s\n", d.key, d.data)
+	for key, value := range c.data {
+		fmt.Printf("%s: %s\n", key, value)
 	}
 }
 
